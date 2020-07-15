d0f7d357688e96acb66bf2114ff72a4a8b93c9b9
Vicky Roberts <vicki_uk@hotmail.com>
2020-07-15 17:19:54 +0100

    Get tokens from .env

--- server/src/auth.ts
+++ server/src/auth.ts
@@ -2,13 +2,13 @@ import { User } from './entity/User';
 import { sign } from 'jsonwebtoken';
 
 export const createAccessToken = (user: User) => {
-  return sign({ userId: user.id }, 'asdflkjadf', {
+  return sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET!, {
     expiresIn: '15m',
   });
 };
 
 export const createRefreshToken = (user: User) => {
-  return sign({ userId: user.id }, '13rjfdslffd9', {
+  return sign({ userId: user.id }, process.env.REFRESH_TOKEN_SECRET!, {
     expiresIn: '7d',
   });
 };
